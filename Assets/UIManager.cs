9afefd12dec7b13f802de8fc6e310d0e09d7cc0b
Muhammad Helga <muhammadhelga41@gmail.com>
2025-05-13 23:43:34 +0700

    RunnerBunnyGame

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -5,22 +5,25 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreUI;
     [SerializeField] private GameObject startMenuUI;
+    [SerializeField] private TextMeshProUGUI lastScore;
     [SerializeField] private GameObject gameOverUI;
 
     GameManager gm;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gm = GameManager.Instance;
         gm.onGameOver.AddListener(ActivateGameOverUI);
     }
 
-    public void PlayButtonHandler() {
+    public void PlayButtonHandler()
+    {
         gm.StartGame();
     }
 
-    public void ActivateGameOverUI() {
+    public void ActivateGameOverUI()
+    {
         gameOverUI.SetActive(true);
+        lastScore.text = "Final Score : " + gm.PrettyScore();
     }
 
     public void OnGUI()
